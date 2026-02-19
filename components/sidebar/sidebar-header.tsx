72de57ca15470744b8a676f5853a005514cc8017
Ser Foll <sergiofollyvi@gmail.com>
2026-02-19 17:15:44 +0100

    Added: Sidebar header component. Updated: .gitignore ignore test page. Closes #11

---
+++ components/sidebar/sidebar-header.tsx
@@ -0,0 +1,10 @@
+export default function SidebarHeader() {
+  return (
+    <section className="border-b border-neutral-300 p-6">
+      <h2>
+        <span className="block font-bold text-3xl">Webbutiken</span>
+        <span className="text-neutral-500">Admin panel</span>
+      </h2>
+    </section>
+  );
+}
