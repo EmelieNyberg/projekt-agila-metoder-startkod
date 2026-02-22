7694ad37dcffb91fd6cceb8627db71634486e294
Tito <titusokara@gmail.com>
2026-02-23 00:30:00 +0100

    feat: connect Pagination to page.tsx with real data

--- app/page.tsx
+++ app/page.tsx
@@ -2,43 +2,41 @@ import type { ProductsResponse } from "@/lib/types/product";
 import ProductsTable from "@/components/ProductsTable";
 import Sidebar from "@/components/sidebar";
 import Form from "../components/form/form";
+import Pagination from "@/components/pagination";
 
 const API_URL = "http://localhost:4000";
 const defaultLimit = "6";
 
-export default async function Home() {
-  // we use the fetch() method to get the products from the API
-  // in this fetch we sort using _sort and _order and we limit the number of products using _limit
-  // we also use _expand to get the relational category data
-  // we can use the other destructed variables like page, total and so on to create pagination or show info
-  const { products, total, page, pages, limit }: ProductsResponse = await fetch(
-    `${API_URL}/products/?_limit=${defaultLimit}&_sort=id&_order=desc&_expand=category`,
-  ).then((res) => res.json());
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ page?: string }>;
+}) {
+  const resolvedParams = await searchParams;
+  const currentPage = Number(resolvedParams?.page) || 1;
 
-  //console.log(products);
+  const { products, total, page, pages, limit }: ProductsResponse = await fetch(
+    `${API_URL}/products/?_limit=${defaultLimit}&_sort=id&_order=desc&_expand=category&_page=${currentPage}`,
+  ).then((res) => res.json());
 
   return (
     <div
       className="min-h-screen md:grid 
       md:[grid-template-areas:'sidebar_form_form''sidebar_main_main']"
     >
-      {/* Sidebar - fixed width, full height
-      <Sidebar />*/}
       <Sidebar className="sticky top-0 bg-white h-screen md:[grid-area:sidebar] border-r border-r-neutral-300" />
 
-      {/* Header - full width
-      <Header /> */}
-
-      {/* Main content area */}
-      <main className="min-h-screen  md:[grid-area:main] p-6 ">
-        {/* Products Search & Filter */}
+      <main className="min-h-screen md:[grid-area:main] p-6">
         <Form />
-
         <ProductsTable products={products} />
 
-        {/*<div className="">
-          <Pagination />
-        </div>*/}
+        {/* Pagination connected to real data */}
+        <Pagination
+          currentPage={currentPage}
+          totalPages={pages}
+          totalProducts={total}
+          productsPerPage={Number(defaultLimit)}
+        />
       </main>
     </div>
   );
