f4aaef621df0541c00df59fcc91e3dc86a0bd42e
Ser Foll <sergiofollyvi@gmail.com>
2026-02-25 14:04:16 +0100

    fetch product in modal

--- app/@modal/(.)products/edit/[id]/page.tsx
+++ app/@modal/(.)products/edit/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { Modal } from "@/components/global";
+import { Product } from "@/lib/types/product";
 
 export default async function EditModal({
   params,
@@ -7,9 +8,19 @@ export default async function EditModal({
 }) {
   const { id } = await params;
 
+  const res = await fetch(`http://localhost:4000/products/${id}`);
+
+  if (!res.ok)
+    return (
+      <h1>Couldn't not find product, make sure the product id is valid!</h1>
+    );
+
+  const product = (await res.json()) as Product;
+
   return (
     <Modal>
-      <div>Edit: {id}</div>
+      {/* should be replace with edit form/edit page component*/}
+      <div>Edit: {product.title}</div>
     </Modal>
   );
 }
