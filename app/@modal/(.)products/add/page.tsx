9c6654d016af0e851f7f87e31d9b935081564a3f
Ser Foll <sergiofollyvi@gmail.com>
2026-02-27 15:49:16 +0100

    fix: last fix build error

--- app/@modal/(.)products/add/page.tsx
+++ app/@modal/(.)products/add/page.tsx
@@ -1,7 +1,7 @@
 import AddProductForm from "@/components/form/add-product-form";
 import { Modal } from "@/components/global";
 
-export default async function EditModal() {
+export default function EditModal() {
   return (
     <Modal>
       {/* should be replace with edit form/edit page component*/}
--- components/global/modal.tsx
+++ components/global/modal.tsx
@@ -24,24 +24,25 @@ export default function Modal({
   function onDismiss() {
     router.back();
   }
-
   // createPortal: render modal content inside #modal-root element
-  return createPortal(
-    <dialog ref={dialogRef} className="relative z-10" onClose={onDismiss}>
-      <div className="fixed inset-0 flex items-center justify-center bg-gray-900/40 transition-opacity">
-        <div className="mb-4 grid w-fit grid-rows-[auto_1fr] place-self-end rounded-xl bg-neutral-100 p-4">
-          <button
-            type="button"
-            onClick={onDismiss}
-            aria-label="close modal"
-            className="w-fit cursor-pointer place-self-start rounded-md bg-transparent p-2 transition hover:bg-purple-900/85 hover:text-white"
-          >
-            <X />
-          </button>
-          {children}
+  if (typeof document !== "undefined")
+    return createPortal(
+      <dialog ref={dialogRef} className="relative z-10" onClose={onDismiss}>
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-900/40 transition-opacity">
+          <div className="mb-4 grid w-fit grid-rows-[auto_1fr] place-self-end rounded-xl bg-neutral-100 p-4">
+            <button
+              type="button"
+              onClick={onDismiss}
+              aria-label="close modal"
+              className="w-fit cursor-pointer place-self-start rounded-md bg-transparent p-2 transition hover:bg-purple-900/85 hover:text-white"
+            >
+              <X />
+            </button>
+            {children}
+          </div>
         </div>
-      </div>
-    </dialog>,
-    document.querySelector("#modal-root") as HTMLDivElement,
-  );
+      </dialog>,
+      document.querySelector("#modal-root") as HTMLDivElement,
+    );
+  else return <div></div>;
 }
